8b7d1243bf294bf176935dc60b78f73e077fb335
Bhanu Prasad G <bp85@users.noreply.github.com>
2025-07-21 03:35:16 -0400

    update api_url

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class dell_info::params {
   $api_key = undef
   $force = false
   $extra_facts = []
-  $api_url = 'https://api.dell.com/support/v2/assetinfo/warranty/tags.json'
+  $api_url = 'https://apigtwb2c.us.dell.com/PROD/sbil/eapi/v5/asset-entitlements'
   $cache_dir = '/var/cache/facts.d'
   $cache_ttl = 604800
 }
